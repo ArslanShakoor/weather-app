5dc166cafb842b25bddfe4a6d10ff5a1f4a5bfd7
Arslan Shakoor <arslantechielive@gmail.com>
2017-08-02 01:42:54 -0400

    add open weather api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
-const API_KEY="71deca9a23214b5df1e2218d418a58dc";
-const API_URL=`api.openweathermap.org/data/2.5/forecast?app_id=${API_KEY}`;
+const API_KEY='42618fb18bc2866bf817902e8fed182e';
+const ROOT_URL=`http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const  FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(city) {
-	const URL = `${API_URL}& q=${city},us`;
-	coust request = axios.get(URL);
+	const URL = `${ROOT_URL}&q=${city},us`;
+	const request = axios.get(URL);
 	return{
+
 		type:  FETCH_WEATHER
 	}
 }
